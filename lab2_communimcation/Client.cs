c15f9b2e8277ffcb9494dfa72e8ff51291be65a7
shirulaf <shirfr@post.bgu.ac.il>
2017-01-09 23:18:21 +0200

    lab 2 in communication - 1 cs TCP

---
+++ lab2_communimcation/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_communimcation
+{
+    class Client
+    {
+        bool m_tx;
+
+    }
+}
---
+++ lab2_communimcation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace lab2_communimcation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry myIP = Dns.Resolve(Dns.GetHostName());
+            IPAddress ip = myIP.AddressList[0];
+            IPEndPoint ipEnd = new IPEndPoint(ip, 6000);
+
+            MyTcpClient mc = new MyTcpClient();
+            MyTcpListener ml = new MyTcpListener();
+
+
+
+            ml.Listen(6000, ip);
+            mc.Connect("HELLOO!!", ip, 6000);
+        }
+    }
+}
